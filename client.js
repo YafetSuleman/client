15fdf3e95d458e6490c3151fed7e0c8f806690cd
Yafet Suleman <yafet.suleman.ys@gmail.com>
2023-05-08 17:46:37 +0200

    clean up

--- client.js
+++ client.js
@@ -9,10 +9,6 @@ const apiProxy = createProxyMiddleware("/api", {
 app.use(express.static("public"));
 app.use(apiProxy);
 
-// app.get("/", (_req, res) => {
-//     res.sendFile("./public/home.html", { root: __dirname });
-// });
-
 app.listen(3000, function () {
     console.log("Client listening on port 3000");
 });
